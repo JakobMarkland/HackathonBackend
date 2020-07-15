12c2820ee6fddb25d224412f4fa33ef091cfbefe
JakobMarkland <jakobmarkland1313@gmail.com>
2020-07-15 19:56:17 -0400

    Adding files to github repository

---
+++ demo/src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.model.User;
+import com.example.demo.model.VerificationToken;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.VerificationTokenRepository;
+
+import lombok.AllArgsConstructor;
+
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+	
+	private final PasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
+	private final VerificationTokenRepository verificationTokenRepository;
+	
+	@Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+        
+        userRepository.save(user);
+        
+        String token = generateVerificationToken(user);
+    }
+	
+	private String generateVerificationToken(User user) {
+		String token = UUID.randomUUID().toString();
+		VerificationToken verificationToken = new VerificationToken();
+		verificationToken.setToken(token);
+		verificationToken.setUser(user);
+		
+		verificationTokenRepository.save(verificationToken);
+		return token;
+		
+		
+		
+	}
+}
